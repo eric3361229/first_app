e2adc6c3269f903f78d5237efaf2cb3f09850f4a
eric3361229 <eric3361229@github.com>
2013-10-26 23:25:55 +0800

    show Ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+ #has_secure_password
+   validates :name, presence:true,length:{minimum:5,maximum: 50}
+   
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+                presence: true,
+                format: { with: VALID_EMAIL_REGEX },uniqueness:true
+
 end
